8aeb16bcb491840f47db7d26d10fdffadca1f0fb
ChewbaccaChew <bobbydjunk@list.ru>
2022-09-16 18:23:02 +0300

    Added restrictions on uploading images

--- WebStore/main/models.py
+++ WebStore/main/models.py
@@ -1,10 +1,25 @@
+from PIL import Image
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+# зависимости для обрезания загружаемого изображения
+# import sys
+# from django.core.files.uploadedfile import InMemoryUploadedFile
+# from io import BytesIO
+
 # использовать юзера, который указан в settings.AUTH_USER_MODEL
 User = get_user_model()
+
+
+class MinResolutionErrorException(Exception):
+    pass
+
+
+class MaxResolutionErrorException(Exception):
+    pass
 
 
 class LatestProductsManager:
@@ -43,6 +58,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -55,6 +74,31 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorException('Разрешение изображения больше максимального!')
+
+        # обрезание загружаемого изображения
+        # image =self.image
+        # img = Image.open(image)
+        # new_img = img.convert('RGB')
+        # resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
+        # filestream = BytesIO()
+        # resized_new_img.save(filestream, 'JPEG', quality=90)
+        # filestream.seek(0)
+        # name = '{}.{}'.format(*self.image.name.split('.'))
+        # self.image = InMemoryUploadedFile(
+        #     filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(file_), None
+        # )
+
+        super().save(*args, **kwargs)
 
 
 class Notebook(Product):
